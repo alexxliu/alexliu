077fc28ae53dec2943338b9eaa400ad9ff1ef29c
Alex Liu <alex369888@gmail.com>
2024-10-07 19:52:11 -0400

    add home page

--- liualex/src/App.js
+++ liualex/src/App.js
@@ -1,9 +1,13 @@
 import Navbar from './containers/navbar'
+import Home from './containers/home'
 
 function App() {
   return (
-    <Navbar></Navbar>
-    
+    <div>
+      <Navbar></Navbar>
+      <Home></Home>
+    </div>
+
   );
 }
 
--- liualex/src/containers/navbar.js
+++ liualex/src/containers/navbar.js
@@ -5,20 +5,23 @@ const Navbar = () => {
     <nav className="bg-blue-600 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-2xl font-bold">
-          MyWebsite
+          alex liu
         </div>
         <ul className="flex space-x-4 text-white">
           <li>
-            <a href="#home" className="hover:text-gray-300">Home</a>
+            <a href="#home" className="hover:text-gray-300">home</a>
           </li>
           <li>
-            <a href="#about" className="hover:text-gray-300">About</a>
+            <a href="#about" className="hover:text-gray-300">about</a>
           </li>
           <li>
-            <a href="#projects" className="hover:text-gray-300">Projects</a>
+            <a href="#experience" className="hover:text-gray-300">experience</a>
           </li>
           <li>
-            <a href="#contact" className="hover:text-gray-300">Contact</a>
+            <a href="#projects" className="hover:text-gray-300">projects</a>
+          </li>
+          <li>
+            <a href="#contact" className="hover:text-gray-300">contact</a>
           </li>
         </ul>
       </div>
