a13d4be83d38afe80e7b2cc6e348b873557ee4a8
Alex Liu <alex369888@gmail.com>
2024-10-12 18:18:44 -0400

    arrow

--- liualex/src/containers/about.js
+++ liualex/src/containers/about.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import Dinosaur from '../images/dinosaurPaint.png'
+import Arrow from '../images/arrowDown.png'
 
 const About = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen space-x-16">
-      <img
-        src={Dinosaur}
-        alt="Your Name"
-        className="w-80 h-80 rounded-full mb-4 shadow-lg"
-      />
-      <div id="about" className="flex flex-col items-center justify-center w-2/5 p-4 bg-white">
-        <h1 className="text-3xl font-bold text-gray-800">about me</h1>
-        <br/>
-        <div className="mt-2 text-lg text-gray-600 text-left">
-          <p>
-              i'm currently a junior in computer science at purdue university, and an incoming software engineering intern at _____.
-          </p>
+    <div className="flex flex-col items-center min-h-screen space-x-16">
+      <div className="flex items-center justify-center ">
+        <img
+          src={Dinosaur}
+          alt="Your Name"
+          className="w-80 h-80 rounded-full outline outline-cyan-500 mb-4 shadow-lg"
+        />
+        <div id="about" className="flex flex-col items-center justify-center w-2/5 p-4 bg-white">
+          <h1 className="text-3xl font-bold text-cyan-500">about me</h1>
           <br/>
-          <p>
-              i also like cooking, eating desserts, anime/manga, music, video games, digital art, and traveling.
-          </p>
-          <br/>
-          <p>
-              if you have any common interests or just wanna chat, please <a href="#contact" className="text-blue-500">reach out</a>!
-          </p>
+          <div className="mt-2 text-lg text-gray-700 text-left">
+            <p>
+                i'm currently a junior in computer science at purdue university, and an incoming software engineering intern at _____.
+            </p>
+            <br/>
+            <p>
+                i also like cooking, eating desserts, anime/manga, music, video games, digital art, and traveling.
+            </p>
+            <br/>
+            <p>
+                if you have any common interests or just wanna chat, please <a href="#contact" className="text-blue-500">reach out</a>!
+            </p>
+          </div>
         </div>
       </div>
+      <img
+            src={Arrow}
+            className="w-48 h-48 fill-cyan-500"
+      />
     </div>
   );
 };
--- liualex/src/containers/projects.js
+++ liualex/src/containers/projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <div id="projects" className="flex flex-col items-center justify-center min-h-screen p-4 bg-white">
       <h1 className="text-3xl font-bold text-gray-800">projects section</h1>
-      <p className="mt-2 text-lg text-gray-600 text-center">
+      <p className="mt-2 text-lg text-gray-700 text-center">
         idk i have some projects right lol
       </p>
     </div>
