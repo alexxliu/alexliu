ca94e675dddb4152c04d50e6c59cba9e2cd3d2b7
Alex Liu <alex369888@gmail.com>
2024-10-14 14:58:46 -0400

    experience

--- liualex/src/containers/experience.js
+++ liualex/src/containers/experience.js
@@ -5,13 +5,13 @@ const Experience = () => {
     <div id="experience" className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-3xl font-bold text-cyan-500">experience</h1>
       <p className="mt-2 text-lg text-gray-700 text-center">
-        ge transportation
+        ge transportation, a wabtec company
       </p>
       <p className="mt-2 text-lg text-gray-700 text-center">
         nationwide
       </p>
       <p className="mt-2 text-lg text-gray-600 text-center">
-        etc
+        flex lab
       </p>
     </div>
   );
