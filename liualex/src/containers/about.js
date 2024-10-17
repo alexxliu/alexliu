833dcfcd970d04581a6cd3b50b89533a5e0f0276
Alex Liu <alex369888@gmail.com>
2024-10-17 02:19:14 -0400

    name

--- liualex/src/containers/about.js
+++ liualex/src/containers/about.js
@@ -5,11 +5,16 @@ import Arrow from '../images/arrowDown.png'
 const About = () => {
   return (
       <div className="flex flex-col sm:flex-row items-center items-center justify-center min-h-screen sm:space-x-16">
-        <img
-          src={Dinosaur}
-          alt="Your Name"
-          className="w-48 h-48 sm:w-80 sm:h-80 rounded-full outline outline-cyan-500 mb-4 shadow-lg"
-        />
+        <div className="flex flex-col">
+          <img
+            src={Dinosaur}
+            alt="Your Name"
+            className="w-48 h-48 sm:w-80 sm:h-80 rounded-full outline outline-cyan-500 mb-4 shadow-lg"
+          />
+          <h1 className="flex justify-center text-6xl font-bold test-gray-800">
+            alex liu
+          </h1>
+        </div>
         <div id="about" className="flex flex-col items-center justify-center w-2/5 p-4 bg-white">
           <h1 className="text-4xl font-bold text-cyan-500">about me</h1>
           <br/>
