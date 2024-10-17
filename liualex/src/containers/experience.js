caeb77820abe45ce6be4afb8ebbbaa11240012f7
Alex Liu <alex369888@gmail.com>
2024-10-16 23:57:52 -0400

    mystery company

--- liualex/src/containers/experience.js
+++ liualex/src/containers/experience.js
@@ -5,6 +5,9 @@ const Experience = () => {
     <div id="experience" className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-3xl font-bold text-cyan-500">experience</h1>
       <p className="mt-2 text-lg text-gray-700 text-center">
+        mystery company
+      </p>
+      <p className="mt-2 text-lg text-gray-700 text-center">
         ge transportation, a wabtec company
       </p>
       <p className="mt-2 text-lg text-gray-700 text-center">
